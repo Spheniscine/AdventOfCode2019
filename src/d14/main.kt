dccb7c807fb85be6447e57ba4e60814ec7b3cca9
Spheniscine <spheniscine@gmail.com>
2019-12-13 21:49:48 +0800

    Day 14 prep

---
+++ src/d14/main.kt
@@ -0,0 +1,16 @@
+package d14
+
+import java.io.File
+
+private val input by lazy { File("src/d14/input/test.in").readText() }
+
+fun main() {
+    println("--- Day 14 ---")
+
+
+    val ans1 = input
+    println("Part 1: $ans1")
+
+//    val ans2 = 2
+//    println("Part 2: $ans2")
+}
