fe8b97eac4f2580c5a340b25afbeaf6fba9e5ab1
spheniscine <spheniscine@gmail.com>
2019-12-19 15:19:08 +0800

    remove memoization, cause not needed

--- src/d19/main.kt
+++ src/d19/main.kt
@@ -12,32 +12,34 @@ fun main() {
     markTime()
     val prog = input.split(',').map { it.toLong() }
 
-    val grid = HashMap<Vec2, Boolean>().memoize { (x, y) ->
+    fun grid(x: Int, y: Int): Boolean {
         val vm = IntCodeVM(prog)
         vm.input(x)
         vm.input(y)
         vm.execute()
-        vm.output.pop() == 1L
+        return vm.output.pop() == 1L
     }
+
+    fun grid(pos: Vec2) = grid(pos.x, pos.y)
 
     var lastTrue = Vec2.ORIGIN
 
-    val ans1 = Rect(0 until 50, 0 until 50).count { pos -> grid[pos].also { if(it) lastTrue = pos } }
+    val ans1 = Rect(0 until 50, 0 until 50).count { pos -> grid(pos).also { if(it) lastTrue = pos } }
     println("Part 1: $ans1")
     printTime()
 
-    //displayGrid(50, 50) { x, y -> grid[x, y] }
+    //displayGrid(50, 50) { x, y -> grid(x, y) }
 
     markTime()
     var (x, y) = lastTrue
-    while(grid[x+1, y]) x++
+    while(grid(x+1, y)) x++
     while(true) {
-        if(x >= 99 && grid[x - 99, y + 99]) {
+        if(x >= 99 && grid(x - 99, y + 99)) {
             x -= 99
             break
         }
         y++
-        while(grid[x+1, y]) x++
+        while(grid(x+1, y)) x++
     }
 
     val ans2 = x * 10000 + y
